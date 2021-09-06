890517eb7a099b4c039f2b68f12c665d7ba51e2f
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-09-06 19:29:08 +0200

    [AUTHENTICATION] Add index.ts file

---
+++ src/authentication/index.ts
@@ -0,0 +1,5 @@
+export * from './guards/jwt-access-token.guard';
+export * from './guards/jwt-refresh-token.guard';
+
+export * from './passport/jwt-access-token.strategy';
+export * from './passport/jwt-refresh-token.strategy';
