e8c3e397677067fcfc4b8810022a5653818a717e
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-09-06 18:51:52 +0200

    [MODULES] Removed all modules to children

--- src/modules/entities/devices/devices.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DevicesService } from './devices.service';
-
-@Module({
-	providers: [DevicesService],
-	exports: [DevicesService],
-})
-export class DevicesModule {}
--- src/modules/entities/workplaces/workplaces.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WorkplacesService } from './workplaces.service';
-
-@Module({
-  providers: [WorkplacesService]
-})
-export class WorkplacesModule {}
