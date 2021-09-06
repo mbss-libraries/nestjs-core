e9644691a0cfc9e429a9eb9336aae20b2ee164ba
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-09-06 20:18:32 +0200

    [HELPERS] Remove TModelNames

--- src/helpers/declaration.helper.ts
+++ src/helpers/declaration.helper.ts
@@ -1,36 +1,18 @@
 import { BaseEntity } from 'typeorm';
 import { Request as BaseRequest } from 'express';
 
-export type IRequestModelsExists = {
-	exists?: TModelsExists;
+export type IRequestModelsExists<T extends string> = {
+	exists?: TModelsExists<T>;
 };
-export type TModelsExists = {
-	[key in TModelNames]?: string[];
+export type TModelsExists<T extends string> = {
+	[key in T]?: string[];
 };
-export type TModelsExistsUpdatedAt = {
-	[key in TModelNames]?: { [key: string]: string };
+export type TModelsExistsUpdatedAt<T extends string> = {
+	[key in T]?: { [key: string]: string };
 };
 
-export type TModelNames = 'App' | 'Location' | 'Project' | 'User';
-// export enum EModelNames {
-//   User = 'User',
-
-//   Tag = 'Tag',
-//   Question = 'Question',
-
-//   Department = 'Department',
-//   Car = 'Car',
-//   CarRoom = 'CarRoom',
-//   CarRoomItem = 'CarRoomItem',
-//   Equipment = 'Equipment',
-// }
-
-export type IResponseModels = {
-	[key in TModelNames]?: BaseEntity[];
-};
-export interface IResponseIncludeModels extends IResponseModels {
-	override?: TModelNames[];
-	delete?: { [key in TModelNames]?: string[] };
-}
+export type IResponseIncludeModels<T extends string> = {
+	[key in T]?: BaseEntity[];
+} & { override?: T[]; delete?: { [key in T]?: string[] } };
 
 export type TRequest = BaseRequest;
