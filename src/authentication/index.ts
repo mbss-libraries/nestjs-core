77a32cd28fde05aa4306b4752e536be88afcd635
Michael Bigalke <michael.bigalke@zalando.de>
2021-09-27 12:51:40 +0200

    Update index.ts

--- src/authentication/index.ts
+++ src/authentication/index.ts
@@ -1,5 +1,3 @@
-export * from './guards/jwt-access-token.guard';
 export * from './guards/jwt-refresh-token.guard';
 
-export * from './passport/jwt-access-token.strategy';
 export * from './passport/jwt-refresh-token.strategy';
