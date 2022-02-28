82e3a04aa14e4e2a54a8c7382ee65353cc519793
GhostLabs94 <12091603+GhostLabs94@users.noreply.github.com>
2022-03-01 00:34:45 +0100

    Update cors.environment.ts

--- src/environments/cors.environment.ts
+++ src/environments/cors.environment.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsConfig: CorsOptions = {
-	origin: ['http://127.0.0.1:8080', 'http://localhost:8080'],
+	origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://localhost:3000'],
 	credentials: true,
 };
